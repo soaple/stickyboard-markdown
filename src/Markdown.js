e5ca70540b1c2f418b7bdb9ac918326b7ab6f944
changgeonLee <changgeon718@gmail.com>
2020-09-11 00:21:45 +0900

    Add save and edit mode

--- src/Markdown.js
+++ src/Markdown.js
@@ -4,6 +4,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
+import TextareaAutosize from 'react-textarea-autosize';
 
 const Wrapper = styled.div`
     height: 100%;
@@ -17,25 +18,96 @@ const Wrapper = styled.div`
     overflow: auto;
 `;
 
+const ButtonWrapeer = styled.div`
+    font: 1em;
+    text-align: right;
+    position: fix;
+`;
+
+const Button = styled.button`
+    background-color: #5287FA; 
+    color: black;
+    border: none;
+    border-radius: 3px;
+`;
+
+const Editer = styled(TextareaAutosize)`
+    resize: none;
+    background-color:transparent; 
+    border: 0;
+    &:focus{
+      outline: none;
+    }
+`;
+
 class Markdown extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+            buttonType: false,
+            buttonLabel : 'Edit',
+            content : this.props.content
+        };
+    }
+
+    buttonClick() {
+      const { content, buttonType } = this.state;
+      const { onSave } = this.props;  
+
+      if(buttonType){
+        onSave(content);
+        this.setState({
+          buttonType: false,
+          buttonLabel: 'Edit'
+        });
+      }else{
+        this.setState({
+          buttonType: true,
+          buttonLabel: 'Save'
+        })
+      }
+    }
+
+    getEditType(){
+      const { content, buttonType } = this.state;
+
+      if(buttonType){
+        return (
+          <Editer 
+            onChange={(e) => {
+              this.setState({
+                content: e.target.value
+              })
+            }}
+            value={content} />
+        );
+      }else{
+        return (<ReactMarkdown source={content} />);
+      }
     }
 
     render() {
-        const { content } = this.props;
+        const { buttonLabel } = this.state;     
+        const editType = this.getEditType();
+
         return (
           <Wrapper>
-            <ReactMarkdown source={content}/>
+            <ButtonWrapeer>
+              <Button onClick={() => this.buttonClick()}>
+                {buttonLabel}
+              </Button>              
+            </ButtonWrapeer>
+
+            { editType }
           </Wrapper>
         );
     }
 }
 
 Markdown.propTypes = {
-  content: PropTypes.string
+  content: PropTypes.string,
+  onSave : PropTypes.func
 };
 
 export default Markdown;
